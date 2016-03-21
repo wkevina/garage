4a552b626c2bd08a768068d74caa3e27c4808119
Kevin Ward <antiomiae@gmail.com>
2016-03-21 16:37:01 -0500

    Render login using template.

--- login.py
+++ login.py
@@ -4,11 +4,8 @@ import auth
 
 class LoginHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('<html><body><form action="/login" method="post">'
-                   'Name: <input type="text" name="name">'
-                   'Password: <input type="password" name="password">'
-                   '<input type="submit" value="Sign in">'
-                   '</form></body></html>')
+        self.render('login.html')
+
 
     def post(self):
         username = self.get_argument('name')
