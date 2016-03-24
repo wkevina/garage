fbb6bb0656d59925761ede1462b39f93facb9d68
Kevin Ward <kevinward@fescorp.com>
2016-03-24 17:37:23 -0500

    Hack attempt to reconnect websocket

--- assets/refresh.js
+++ assets/refresh.js
@@ -34,19 +34,47 @@ window.addEventListener('load', function() {
 
     var captureImg = document.getElementById(imgId);
 
+    function openSocket() {
+        return new WebSocket(wsProtocol + host + '/socket');
+    }
+
+
     if (captureImg !== null) {
-        var ws = new WebSocket(wsProtocol + host + '/socket');
+        var ws;
 
-        ws.addEventListener('message', function(evt) {
-            loadImage('/capture/?' + new Date().getTime()).
-                then(function(newImg) {
-                    var oldImg = document.getElementById(imgId);
-                    oldImg.src = newImg.src;
+        function connectSocket() {
+            try {
+                ws = openSocket();
+
+                ws.addEventListener('open', function(evt) {
+                    ws.addEventListener('message', function(evt) {
+                        loadImage('/capture/?' + new Date().getTime()).
+                            then(function(newImg) {
+                                var oldImg = document.getElementById(imgId);
+                                oldImg.src = newImg.src;
+                            });
+                    });
+
+                    window.addEventListener('unload', function() {
+                        ws.close();
+                    });
                 });
-        });
 
-        window.addEventListener('unload', function() {
-            ws.close();
-        });
+                ws.addEventListener('close', function(evt) {
+                    window.setTimeout(checkConnection, 1000);
+                });
+            } catch (ex) {
+                window.setTimeout(checkConnection, 1000);
+            }
+        }
+
+        function checkConnection() {
+            if (ws && ws.readyState != WebSocket.OPEN) {
+                connectSocket();
+            }
+        }
+
+        connectSocket();
+        window.setTimeout(checkConnection, 1000);
     }
 });
