25a6e905b8f5d154f603bdcda3f634c5efca4333
Kevin Ward <kevinward@fescorp.com>
2016-03-25 17:45:28 -0500

    Rewrite refresh.js yet again.
    Implemented a reconnecting wrapper around WebSocket. Uses component/emitter library for emitter interface.

--- assets/refresh.js
+++ assets/refresh.js
@@ -1,80 +1,134 @@
 window.addEventListener('load', function() {
-
     var imgId = 'capture',
-        host = window.location.host,
-        protocol = window.location.protocol,
-        wsProtocol;
+        as = new AutoSocket('/socket');
 
-    if (protocol == 'http:') {
-        wsProtocol = 'ws://';
-    } else if (protocol == 'https:') {
-        wsProtocol = 'wss://';
-    }
+    as.on('message', function() {
+        loadImage('/capture/?' + new Date().getTime()).
+            then(function(newImg) {
+                var oldImg = document.getElementById(imgId);
+                oldImg.src = newImg.src;
+            });
+    });
+});
 
-    function loadImage(url) {
-        var newImg = new Image(),
-            deferred = new Promise(function (resolve, reject) {
-                newImg.src = url;
+function loadImage(url) {
+    var newImg = new Image(),
+        deferred = new Promise(function (resolve, reject) {
+            newImg.src = url;
 
-                newImg.addEventListener('load', function() {
-                    resolve(newImg);
-                });
-
-                newImg.addEventListener('error', function() {
-                    reject(newImg);
-                });
-
-                newImg.addEventListener('abort', function() {
-                    reject(newImg);
-                });
+            newImg.addEventListener('load', function() {
+                resolve(newImg);
             });
 
-        return deferred;
+            newImg.addEventListener('error', function() {
+                reject(newImg);
+            });
+
+            newImg.addEventListener('abort', function() {
+                reject(newImg);
+            });
+        });
+
+    return deferred;
+}
+
+function AutoSocket(path) {
+    this.path = path;
+    this.socket = null;
+    this.timeout = null;
+
+    this.onOpen = this.onOpen.bind(this);
+    this.onClose = this.onClose.bind(this);
+    this.onError = this.onError.bind(this);
+    this.onMessage = this.onMessage.bind(this);
+
+    this.connect();
+}
+
+AutoSocket.prototype.connect = function() {
+    if (!this.socket) {
+        var socket = new WebSocket(this.getUri());
+        this.socket = socket;
+        this.socket.addEventListener('open', this.onOpen);
+        this.socket.addEventListener('close', this.onClose);
+        this.socket.addEventListener('error', this.onError);
+        this.socket.addEventListener('message', this.onMessage);
     }
 
-    var captureImg = document.getElementById(imgId);
+    this.timeout = window.setTimeout(this.reconnect.bind(this), 5000);
+};
 
-    function openSocket() {
-        return new WebSocket(wsProtocol + host + '/socket');
+AutoSocket.prototype.reconnect = function() {
+    this.timeout = null;
+    if (this.socket) {
+        this.socket.close();
+        this.socket = null;
     }
+    this.connect();
+};
 
+AutoSocket.prototype.getUri = function() {
+    var host = window.location.host,
+        protocol = this.getProtocol(),
+        uri = '';
 
-    if (captureImg !== null) {
-        var ws;
+    uri += protocol;
+    uri += '//';
+    uri += host;
+    uri += this.path;
 
-        function connectSocket() {
-            try {
-                ws = openSocket();
+    return uri;
+};
 
-                ws.addEventListener('open', function(evt) {
-                    ws.addEventListener('message', function(evt) {
-                        loadImage('/capture/?' + new Date().getTime()).
-                            then(function(newImg) {
-                                var oldImg = document.getElementById(imgId);
-                                oldImg.src = newImg.src;
-                            });
-                    });
+AutoSocket.prototype.getProtocol = function() {
+    var protocol = window.location.protocol;
 
-                    window.addEventListener('unload', function() {
-                        ws.close();
-                    });
-                });
-
-                ws.addEventListener('close', function(evt) {
-                    window.setTimeout(checkConnection, 1000);
-                });
-            } catch (ex) {
-                window.setTimeout(checkConnection, 1000);
-            }
-        }
-
-        function checkConnection() {
-            if (ws && ws.readyState != WebSocket.OPEN) {
-                connectSocket();
-            }
-        }
-
-        connectSocket();
-        window.setTimeout(checkConnection, 1000);
+    if (protocol == 'http:') {
+        return 'ws:';
+    } else if (protocol == 'https:') {
+        return 'wss:';
+    } else {
+        console.log(
+            "I don't know how this could happen. What kind of protocol is " +
+                protocol + "?"
+        );
+        return 'ws:';
     }
-});
+};
+
+AutoSocket.prototype.onOpen = function() {
+    if (this.timeout) {
+        window.clearTimeout(this.timeout);
+        this.timeout = null;
+    }
+    // Emit event to subscribers
+    this.emit('open');
+};
+
+AutoSocket.prototype.onClose = function() {
+    if (!this.timeout) {
+        this.reconnect();
+    }
+    // Emit event to subscribers
+    this.emit('close');
+};
+
+AutoSocket.prototype.onError = function() {
+    // Emit event to subscribers
+    this.emit('error');
+};
+
+AutoSocket.prototype.onMessage = function(msg) {
+    // Emit event to subscribers
+    this.emit('message', msg);
+};
+
+AutoSocket.prototype.send = function(msg) {
+    // Submit data to socket
+    if (this.socket && this.socket.readyState == WebSocket.OPEN) {
+        this.socket.send(msg);
+    }
+};
+
+// Mixin Emitter
+Emitter(AutoSocket.prototype);
