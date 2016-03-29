9bfbb85ada0cd73df509d72aef1ddbb46029d9a2
Kevin Ward <kevinward@fescorp.com>
2016-03-29 13:41:47 -0500

    Use JSPM for package management. ES6-ify code to fit in.

---
+++ assets/autosocket.js
@@ -0,0 +1,105 @@
+import Emitter from 'component/emitter';
+
+export class AutoSocket {
+    constructor(path) {
+        this.path = path;
+        this.socket = null;
+        this.timeout = null;
+
+        this.onOpen = this.onOpen.bind(this);
+        this.onClose = this.onClose.bind(this);
+        this.onError = this.onError.bind(this);
+        this.onMessage = this.onMessage.bind(this);
+
+        this.connect();
+    }
+
+    connect() {
+        if (!this.socket) {
+            var socket = new WebSocket(this.getUri());
+            this.socket = socket;
+            this.socket.addEventListener('open', this.onOpen);
+            this.socket.addEventListener('close', this.onClose);
+            this.socket.addEventListener('error', this.onError);
+            this.socket.addEventListener('message', this.onMessage);
+        }
+
+        this.timeout = window.setTimeout(this.reconnect.bind(this), 5000);
+    };
+
+    reconnect() {
+        this.timeout = null;
+        if (this.socket) {
+            this.socket.close();
+            this.socket = null;
+        }
+        this.connect();
+    };
+
+    getUri() {
+        var host = window.location.host,
+            protocol = this.getProtocol(),
+            uri = '';
+
+        uri += protocol;
+        uri += '//';
+        uri += host;
+        uri += this.path;
+
+        return uri;
+    };
+
+    getProtocol() {
+        var protocol = window.location.protocol;
+
+        if (protocol == 'http:') {
+            return 'ws:';
+        } else if (protocol == 'https:') {
+            return 'wss:';
+        } else {
+            console.log(
+                "I don't know how this could happen. What kind of protocol is " +
+                    protocol + "?"
+            );
+            return 'ws:';
+        }
+    };
+
+    onOpen() {
+        if (this.timeout) {
+            window.clearTimeout(this.timeout);
+            this.timeout = null;
+        }
+        // Emit event to subscribers
+        this.emit('open');
+    };
+    onClose() {
+        if (!this.timeout) {
+            this.reconnect();
+        }
+        // Emit event to subscribers
+        this.emit('close');
+    };
+
+    onError() {
+        // Emit event to subscribers
+        this.emit('error');
+    };
+
+    onMessage(msg) {
+        // Emit event to subscribers
+        this.emit('message', msg);
+    };
+
+    send(msg) {
+        // Submit data to socket
+        if (this.socket && this.socket.readyState == WebSocket.OPEN) {
+            this.socket.send(msg);
+        }
+    };
+}
+
+// Mixin Emitter
+Emitter(AutoSocket.prototype);
+
+export default AutoSocket;
--- assets/refresh.js
+++ assets/refresh.js
@@ -1,4 +1,7 @@
-window.addEventListener('load', function() {
+import AutoSocket from 'autosocket.js';
+
+function startRefresh() {
+    console.log('load');
     var imgId = 'capture',
         as = new AutoSocket('/socket');
 
@@ -9,7 +12,7 @@ window.addEventListener('load', function() {
                 oldImg.src = newImg.src;
             });
     });
-});
+}
 
 function loadImage(url) {
     var newImg = new Image(),
@@ -32,103 +35,8 @@ function loadImage(url) {
     return deferred;
 }
 
-function AutoSocket(path) {
-    this.path = path;
-    this.socket = null;
-    this.timeout = null;
-
-    this.onOpen = this.onOpen.bind(this);
-    this.onClose = this.onClose.bind(this);
-    this.onError = this.onError.bind(this);
-    this.onMessage = this.onMessage.bind(this);
-
-    this.connect();
+if (document.readyState !== 'complete') {
+    window.addEventListener('DOMContentLoaded', startRefresh);
+} else {
+    startRefresh();
 }
-
-AutoSocket.prototype.connect = function() {
-    if (!this.socket) {
-        var socket = new WebSocket(this.getUri());
-        this.socket = socket;
-        this.socket.addEventListener('open', this.onOpen);
-        this.socket.addEventListener('close', this.onClose);
-        this.socket.addEventListener('error', this.onError);
-        this.socket.addEventListener('message', this.onMessage);
-    }
-
-    this.timeout = window.setTimeout(this.reconnect.bind(this), 5000);
-};
-
-AutoSocket.prototype.reconnect = function() {
-    this.timeout = null;
-    if (this.socket) {
-        this.socket.close();
-        this.socket = null;
-    }
-    this.connect();
-};
-
-AutoSocket.prototype.getUri = function() {
-    var host = window.location.host,
-        protocol = this.getProtocol(),
-        uri = '';
-
-    uri += protocol;
-    uri += '//';
-    uri += host;
-    uri += this.path;
-
-    return uri;
-};
-
-AutoSocket.prototype.getProtocol = function() {
-    var protocol = window.location.protocol;
-
-    if (protocol == 'http:') {
-        return 'ws:';
-    } else if (protocol == 'https:') {
-        return 'wss:';
-    } else {
-        console.log(
-            "I don't know how this could happen. What kind of protocol is " +
-                protocol + "?"
-        );
-        return 'ws:';
-    }
-};
-
-AutoSocket.prototype.onOpen = function() {
-    if (this.timeout) {
-        window.clearTimeout(this.timeout);
-        this.timeout = null;
-    }
-    // Emit event to subscribers
-    this.emit('open');
-};
-
-AutoSocket.prototype.onClose = function() {
-    if (!this.timeout) {
-        this.reconnect();
-    }
-    // Emit event to subscribers
-    this.emit('close');
-};
-
-AutoSocket.prototype.onError = function() {
-    // Emit event to subscribers
-    this.emit('error');
-};
-
-AutoSocket.prototype.onMessage = function(msg) {
-    // Emit event to subscribers
-    this.emit('message', msg);
-};
-
-AutoSocket.prototype.send = function(msg) {
-    // Submit data to socket
-    if (this.socket && this.socket.readyState == WebSocket.OPEN) {
-        this.socket.send(msg);
-    }
-};
-
-// Mixin Emitter
-Emitter(AutoSocket.prototype);
